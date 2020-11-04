c89803e319598c4095be935efb4e1180f156600b
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2020-11-04 01:01:06 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 // Made by YouWalkedOff, do not steal and claim it's made by you! 
 let msg='YouWalkedOff';document.querySelector('.header').addEventListener('mouseover',e=>{console.log(e);document.querySelector('.header').innerHTML=msg+' <i class="fas fa-toggle-off"></i>'});document.querySelector('.header').addEventListener('mouseout',e=>{document.querySelector('.header').innerHTML=msg+' <i class="fas fa-toggle-on"></i>'});for(x = 0;x < 15; x++){console.log(x + "// **** Made by YouWalkedOff **** \\" + x)}
+window.onload = function() {
+    var url = new URL((window.location.href).toLowerCase())
+    
+    console.log();
+    switch(url.searchParams.get("selected")){
+        case "portfolio":
+            document.querySelector("#portfolio").innerHTML = "<li-selected>portfolio</li-selected>"
+            break;
+        case "github":
+            document.querySelector("#github").innerHTML = "<li-selected>github</li-selected>"
+            break;
+        case "projects":
+            document.querySelector("#projects").innerHTML = "<li-selected>projects</li-selected>"
+            break;
+        default:
+            switch(Math.floor((Math.random() * 3) + 1)){
+                case 1:
+                    document.querySelector("#portfolio").innerHTML = "<li-selected>portfolio</li-selected>"
+                    break;
+                case 2:
+                    document.querySelector("#github").innerHTML = "<li-selected>github</li-selected>"
+                    break;
+                case 3:
+                    document.querySelector("#projects").innerHTML = "<li-selected>projects</li-selected>"
+                    break;
+            }
+            break;
+                
+            
+    }
+}
